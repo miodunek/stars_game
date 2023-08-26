229c28b8d82f08feff01aa9b5702d377b632a3ed
miodunek <marcelimiodunek@gmail.com>
2023-08-26 22:53:18 +0200

    Transalte comments and clean the code

--- script.js
+++ script.js
@@ -6,23 +6,23 @@ const highScore = document.querySelector('#highScoreValue');
 const audio = document.querySelector('#audio');
 const music = document.querySelector('#music');
 const wrongAudio = document.querySelector('#wrong');
+const musicIcon = document.querySelector('#musicIcon');
 
 let starPosition; 
 let result;
 let starInterval;
 let timeLeft;
 let countDownTimer;
-let canClick = false; //zmienna potrzebna, aby nie można było 2 razy kliknąć na tę samą gwiazdę
+let canClick = false;
 let bestScore = 0; 
 
-//funkcja do włączania i wyłączania muzyki
-//włącza się po załadowaniu strony, aby domyślnie muzyka mogła być wyłączona
+
 window.addEventListener('load', () => {
-  var music = document.querySelector('#music');
-  var musicIcon = document.querySelector('#musicIcon');
 
   music.loop = true;
 
+  //function for turning music on and off
+  //music is automatically off in case it's too loud for the player
   function startMusic() {
     music.play();
     musicIcon.classList.remove('music-off');
@@ -34,7 +34,7 @@ window.addEventListener('load', () => {
     musicIcon.classList.add('music-off');
   }
   
-  //dodanie funkcji włączania i wyłączania muzyki do specjalnej ikonki
+  //music can be turned on/off using icons
   musicIcon.addEventListener('click', () => {
     if (music.paused) {
       startMusic();
@@ -44,80 +44,79 @@ window.addEventListener('load', () => {
     }
   });
 
-  stopMusic(); //automatyczne wyłączenie muzyki, aby nie irytować gracza od razu po załadowaniu strony
+  stopMusic();
 });
 
-//funkcja losująca, w którym miejscu pojawi się gwiazda
+//picking random square for the star
 function randomSquare() {
   squares.forEach(square => {
-    square.classList.remove('star'); //resetowanie położenia gwiazdy
+    square.classList.remove('star');
   });
 
   const randomIndex = Math.floor(Math.random() * squares.length);
   const randomSquare = squares[randomIndex];
-  randomSquare.classList.add('star'); //pokazanie gwiazdy na wylosowanym kwadracie
-  starPosition = randomSquare.id; //id wylosowanego kwadratu podstawiane jest pod zmienną położenia gwiazdy
+  randomSquare.classList.add('star'); //star appears on the square picked before
+  starPosition = randomSquare.id;
   canClick = true;
 }
 
-//funkcja zmieniajaca polozenie gwiazdy co 0.6 sekundy
+//star changes position every 0.6 seconds
 function changePosition() {
   clearInterval(starInterval);
   starInterval = setInterval(randomSquare, 600);
 }
 
-//definicja funkcji rozpoczęcia gry, która wywołana będzie kliknięciem guzika PLAY
+//starting the game after the player presses PLAY button
 function startGame() {
-  button.disabled = true; //uniemożliwienie zaczęcia gry od nowa w jej trakcie
+  button.disabled = true; //disabling the PLAY button after starting the game
   button.classList.add('clicked');
   document.querySelector('body').classList.remove('win');
   document.querySelector('#board').classList.remove('win');
 
-  //resetowanie zmiennych
+  //resetting variables
   result = 0;
   timeLeft = 60;
   points.textContent = result;
   timer.textContent = timeLeft;
-  highScore.textContent = bestScore + "/50"; //zwycięstwo oznacza koniec gry, więc max wynik może wynosić 50
+  highScore.textContent = bestScore + "/50";
   clearInterval(starInterval);
   clearInterval(countDownTimer);
   randomSquare();
   changePosition();
-  countDownTimer = setInterval(countDown, 1000); //odliczanie minuty co 1 sekunde
+  countDownTimer = setInterval(countDown, 1000);
   canClick = true;
 }
 
-//głowny mechanizm działania gry; punkty naliczają się, gdy gracz kliknie kwadrat z oznaczeniem
-//pozycji gwiazdy i możliwością kliknięcia kwadratu uwarunkowaną w poprzednich funkcjach
+//the main game mechanism
 squares.forEach(square => {
   square.addEventListener('click', () => {
-    if (canClick) { //warunek rozpoczęcia całego mechanizmu, aby nie można było klikać na kwadraty przed rozpoczęciem gry
+    if (canClick) { //it's impossible to click on squares and earn points before the game starts
       if (square.id == starPosition && canClick) {
         result++;
         points.textContent = result;
-        canClick = false;  //zapewnienie, że można kliknąć gwiazdę tylko raz
-        playSound(audio); //dźwięk kliknięcia na poprawny kwadrat
+        canClick = false;  //stars can be clicked only once per appearing
+        playSound(audio); //audio for clicking on correct square
       } 
       else {
-        timer.textContent = timeLeft + '-3'; //odejmowanie 3 sekund w razie kliknięcia na zły kwadrat
-        timeLeft = Math.max(0, timeLeft - 3); //zapewnienie, że czas nie spadnie poniżej 0 po odjęciu 3 sekund
+        timer.textContent = timeLeft + '-3'; //substract 3 from the time if wrong square is clicked
+        timeLeft = Math.max(0, timeLeft - 3);
 
-        playSound(wrongAudio); //dźwięk kliknięcia na zły kwadrat
-        //animacja w przypadku kliknięcia złego kwadratu
+        playSound(wrongAudio);
+   
         square.classList.add('incorrect'); 
-        setTimeout(() => { //odczekanie chwili przed usunięciem animacji
+        setTimeout(() => {
           square.classList.remove('incorrect');
         }, 300); 
       }
-      if (result > bestScore) { //aktualizowanie najlepszego wyniku gracza
+      if (result > bestScore) { //updating best score
         bestScore = result;
         highScore.textContent = bestScore + "/50";
       }
-      if (timeLeft === 0) { //warunek przegranej. musi być wywołany drugi raz, bo w countDown() nie uwzględnia się odejmowania 3 sekund
+      if (timeLeft === 0) { //lose condition called second time, bc -3 seconds aren't aren't included in countDown()
         endGame();
         alert('YOU LOSE :( FINAL SCORE: ' + result); 
       }
-      if (result >= 50) { //warunek zwycięstwa
+      if (result >= 50) { //win condition
         win();
         alert('YOU WIN :) FINAL SCORE: ' + result);
       }
@@ -125,18 +124,18 @@ squares.forEach(square => {
   });
 });
 
-//funkcja wyświetlająca pozostały czas
+//shows the remaining time
 function countDown() {
   timeLeft--;
   timer.textContent = timeLeft;
-  //przegrana w razie zakończenia odliczania
+
   if (timeLeft === 0) {
     endGame();
     alert('YOU LOSE :( FINAL SCORE: ' + result);
   }
 }
 
-//funkcja konczaca grę
+//ends the game
 function endGame() {
   clearInterval(countDownTimer);
   clearInterval(starInterval);
@@ -145,22 +144,20 @@ function endGame() {
   timer.textContent = '0';
 }
 
-//funkcja kończąca grę, ale w przypadku zwycięstwa
+//ends the game (win)
 function win() {
   endGame();
   document.querySelector('body').classList.add('win');
   document.querySelector('#board').classList.add('win');
 }
 
-//funkcja do puszczania dźwięków gry
+//plays the game sounds
 function playSound(sound) {
   sound.currentTime = 0;
   sound.play();
 }
 
-
-
-//funkcje do zmiany głośności muzyki i dźwięków gry
+//allows to change the volume of music and sounds
 function changeMusicVolume(musicVolume) {
   music.volume = musicVolume;
 }
@@ -171,5 +168,5 @@ function changeVolume(volume) {
   audio.volume = volume;
   audio.play(); //odtworzenie dźwięku przy każdej zmianie, aby gracz wiedział, jak głośna będzie gra
 }
-//wywołanie rozpoczęcia gry po kliknięciu przycisku PLAY
+//starts the game after pressing the PLAY button
 button.addEventListener('click', startGame);
